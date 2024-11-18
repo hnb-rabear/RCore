2105043a74ff5183b67a842f18333c1b7cf2c442
RadBear <nbhung71711@gmail.com>
2024-11-18 14:43:30 +0700

    Fix haptic touch

--- Assets/RCore/Main/Runtime/Plugins/HapticFeedback.cs
+++ Assets/RCore/Main/Runtime/Plugins/HapticFeedback.cs
@@ -6,32 +6,41 @@ namespace CandyCoded.HapticFeedback
 	{
 		public static void LightFeedback()
 		{
+			try
+			{
 #if UNITY_IOS
-            iOS.HapticFeedback.PerformHapticFeedback("light");
+				iOS.HapticFeedback.PerformHapticFeedback("light");
 #elif UNITY_ANDROID
-			Android.HapticFeedback.PerformHapticFeedback(Android.HapticFeedbackConstants.CLOCK_TICK);
+				Android.HapticFeedback.PerformHapticFeedback(Android.HapticFeedbackConstants.CLOCK_TICK);
 #endif
+			}
+			catch { }
 		}
 
 		public static void MediumFeedback()
 		{
+			try
+			{
 #if UNITY_IOS
-            iOS.HapticFeedback.PerformHapticFeedback("medium");
+				iOS.HapticFeedback.PerformHapticFeedback("medium");
 #elif UNITY_ANDROID
-			Android.HapticFeedback.PerformHapticFeedback(Android.HapticFeedbackConstants.VIRTUAL_KEY);
+				Android.HapticFeedback.PerformHapticFeedback(Android.HapticFeedbackConstants.VIRTUAL_KEY);
 #endif
-
+			}
+			catch { }
 		}
 
 		public static void HeavyFeedback()
 		{
-
+			try
+			{
 #if UNITY_IOS
-            iOS.HapticFeedback.PerformHapticFeedback("heavy");
+				iOS.HapticFeedback.PerformHapticFeedback("heavy");
 #elif UNITY_ANDROID
-			Android.HapticFeedback.PerformHapticFeedback(Android.HapticFeedbackConstants.CALENDAR_DATE);
+				Android.HapticFeedback.PerformHapticFeedback(Android.HapticFeedbackConstants.CALENDAR_DATE);
 #endif
-
+			}
+			catch { }
 		}
 	}
 }
