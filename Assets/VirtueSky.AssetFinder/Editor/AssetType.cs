6e99a9413cc00b6c21c44b480846083318acd7c8
RadBear <nbhung71711@gmail.com>
2024-11-25 23:07:54 +0700

    Add AssetFinder

--- Assets/VirtueSky.AssetFinder/Editor/AssetType.cs
+++ Assets/VirtueSky.AssetFinder/Editor/AssetType.cs
@@ -11,33 +11,33 @@ namespace VirtueSky.AssetFinder.Editor
 
         internal static readonly AssetType[] FILTERS =
         {
-            new AssetType("Scene", ".unity"),
-            new AssetType("Prefab", ".prefab"),
-            new AssetType("Model", ".3df", ".3dm", ".3dmf", ".3dv", ".3dx", ".c5d", ".lwo", ".lws",
+            new("Scene", ".unity"),
+            new("Prefab", ".prefab"),
+            new("Model", ".3df", ".3dm", ".3dmf", ".3dv", ".3dx", ".c5d", ".lwo", ".lws",
                 ".ma", ".mb",
                 ".mesh", ".vrl", ".wrl", ".wrz", ".fbx", ".dae", ".3ds", ".dxf", ".obj", ".skp",
                 ".max", ".blend"),
-            new AssetType("Material", ".mat", ".cubemap", ".physicsmaterial"),
-            new AssetType("Texture", ".ai", ".apng", ".png", ".bmp", ".cdr", ".dib", ".eps",
+            new("Material", ".mat", ".cubemap", ".physicsmaterial"),
+            new("Texture", ".ai", ".apng", ".png", ".bmp", ".cdr", ".dib", ".eps",
                 ".exif", ".ico", ".icon",
                 ".j", ".j2c", ".j2k", ".jas", ".jiff", ".jng", ".jp2", ".jpc", ".jpe", ".jpeg",
                 ".jpf", ".jpg", "jpw",
                 "jpx", "jtf", ".mac", ".omf", ".qif", ".qti", "qtif", ".tex", ".tfw", ".tga",
                 ".tif", ".tiff", ".wmf",
                 ".psd", ".exr", ".rendertexture"),
-            new AssetType("Video", ".asf", ".asx", ".avi", ".dat", ".divx", ".dvx", ".mlv", ".m2l",
+            new("Video", ".asf", ".asx", ".avi", ".dat", ".divx", ".dvx", ".mlv", ".m2l",
                 ".m2t", ".m2ts",
                 ".m2v", ".m4e", ".m4v", "mjp", ".mov", ".movie", ".mp21", ".mp4", ".mpe", ".mpeg",
                 ".mpg", ".mpv2",
                 ".ogm", ".qt", ".rm", ".rmvb", ".wmv", ".xvid", ".flv"),
-            new AssetType("Audio", ".mp3", ".wav", ".ogg", ".aif", ".aiff", ".mod", ".it", ".s3m",
+            new("Audio", ".mp3", ".wav", ".ogg", ".aif", ".aiff", ".mod", ".it", ".s3m",
                 ".xm"),
-            new AssetType("Script", ".cs", ".js", ".boo", ".h"),
-            new AssetType("Text", ".txt", ".json", ".xml", ".bytes", ".sql"),
-            new AssetType("Shader", ".shader", ".cginc"),
-            new AssetType("Animation", ".anim", ".controller", ".overridecontroller", ".mask"),
-            new AssetType("Unity Asset", ".asset", ".guiskin", ".flare", ".fontsettings", ".prefs"),
-            new AssetType("Others") //
+            new("Script", ".cs", ".js", ".boo", ".h"),
+            new("Text", ".txt", ".json", ".xml", ".bytes", ".sql"),
+            new("Shader", ".shader", ".cginc"),
+            new("Animation", ".anim", ".controller", ".overridecontroller", ".mask"),
+            new("Unity Asset", ".asset", ".guiskin", ".flare", ".fontsettings", ".prefs"),
+            new("Others") //
         };
 
         private static AssetFinderIgnore _ignore;
