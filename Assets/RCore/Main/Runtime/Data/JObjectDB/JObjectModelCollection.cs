5059edf0ba66b5e5d448d48eb76e24771082c6e3
RadBear <nbhung71711@gmail.com>
2024-12-10 18:58:05 +0700

    1

--- Assets/RCore/Main/Runtime/Data/JObjectDB/JObjectModelCollection.cs
+++ Assets/RCore/Main/Runtime/Data/JObjectDB/JObjectModelCollection.cs
@@ -1,8 +1,9 @@
 using System;
-using System.Collections;
 using System.Collections.Generic;
 using Newtonsoft.Json;
+#if UNITY_EDITOR
 using RCore.Editor;
+#endif
 using RCore.Inspector;
 using UnityEditor;
 using UnityEngine;
